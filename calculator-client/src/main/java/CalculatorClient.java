d62ab6344b65f3757cb462c57352cca6fe962c73
kurau <kurau@yandex-team.ru>
2017-12-19 13:08:24 +0300

    Create calculator client

---
+++ calculator-client/src/main/java/CalculatorClient.java
@@ -0,0 +1,18 @@
+/**
+ * @author kurau (Yuri Kalinin)
+ */
+public class CalculatorClient {
+
+    public int adding(int a, int b) {
+        return a + b;
+    }
+
+    public int subtracting(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
