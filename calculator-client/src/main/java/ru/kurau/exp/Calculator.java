3f7c9ba0c1e61d9ffdd785a5b517a010f83b07e1
kurau <kurau@yandex-team.ru>
2017-12-19 13:08:24 +0300

    Create calculator

---
+++ calculator-client/src/main/java/ru/kurau/exp/Calculator.java
@@ -0,0 +1,20 @@
+package ru.kurau.exp;
+
+/**
+ * @author kurau (Yuri Kalinin)
+ */
+public class Calculator {
+
+    public long adding(int a, int b) {
+        return a + b;
+    }
+
+    public long subtracting(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
